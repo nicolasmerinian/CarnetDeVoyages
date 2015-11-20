a857e3e7995b9a375e9d3cb990f2294741dae152
Nicolas Merinian <nicolas.merinian@gmail.com>
2015-11-20 18:53:49 +0100

    Création de l'activité créer un voyage

---
+++ app/src/main/java/fr/esiea/kokumo/carnetdevoyages/Activity/CreateTravelActivity.java
@@ -0,0 +1,28 @@
+package fr.esiea.kokumo.carnetdevoyages.Activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import fr.esiea.kokumo.carnetdevoyages.R;
+
+/**
+ * Created by Kokumo on 19/11/2015.
+ */
+public class CreateTravelActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_travel_activity);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
